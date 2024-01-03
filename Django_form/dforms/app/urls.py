00a5c5d01c5682b63ec2c61e769c45e373d1a93b
Himanshu Pal <palhimanshu206243@gmail.com>
2024-01-03 23:55:57 +0530

    form

--- Django_form/dforms/app/urls.py
+++ Django_form/dforms/app/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',School,name='School'),
+    path('Teacher/',Teacher,name='Teacher'),
 ]
 
--- Django_form/dforms/app/views.py
+++ Django_form/dforms/app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from app.forms import SchoolForm,TeacherForm,StudentForm
 
-# Create your views here.
+# Views For School.
 def School(request):
     SNFO = SchoolForm()
     d={'SNFO':SNFO}
@@ -14,3 +14,16 @@ def School(request):
             return HttpResponse('Data is invalid')
         
     return render(request,'school.html',d)
+
+# Views For Teacher.
+def Teacher(request):
+    TNFO = TeacherForm()
+    d={'TNFO':TNFO}
+    if request.method=='POST':
+        TFDO = TeacherForm(request.POST)
+        if TFDO.is_valid():
+            return HttpResponse('Data is Correct')
+        else:
+            return HttpResponse('Data is invalid')
+        
+    return render(request,'teacher.html',d)
