d609da4afa6bf1cee937f1502822e1425a45c528
Himanshu Pal <palhimanshu206243@gmail.com>
2024-01-05 23:03:50 +0530

    djform

--- Django_form/dforms/app/views.py
+++ Django_form/dforms/app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from app.forms import SchoolForm,TeacherForm,StudentForm
-from .models import School,Teacher
+from .models import School,Teacher,Student
 
 # Views For School.
 def school(request):
@@ -15,7 +15,10 @@ def school(request):
             sl=SFDO.cleaned_data['Sc_Location']
             SO=School.objects.get_or_create(Sc_Name=sn,Sc_Principle=sp,Sc_Location=sl)[0]
             SO.save()
-            return HttpResponse('School is Created')
+            # return HttpResponse('School is Created')
+            SCD=School.objects.all()
+            a={'SCD':SCD}
+            return render(request,'schooldata.html',a)
         else:
             return HttpResponse('Data is invalid')
         
@@ -34,7 +37,9 @@ def teacher(request):
             ts=TFDO.cleaned_data['T_Sub']
             TO=Teacher.objects.get_or_create(School=sc,Tname=tn,T_Exp=te,T_Sub=ts)[0]
             TO.save()
-            return HttpResponse('Teacher is created')
+            TD=Teacher.objects.all()
+            b={'TD':TD}
+            return render(request,'teacherdata.html',b)
         else:
             return HttpResponse('Data is invalid')
         
@@ -44,4 +49,22 @@ def teacher(request):
 def student(request):
     UNFO = StudentForm()
     d={'UNFO':UNFO}
+    if request.method=='POST':
+        SNFO = StudentForm(request.POST)
+        if SNFO.is_valid():
+            sc=SNFO.cleaned_data['School']
+            tc=SNFO.cleaned_data['Teachers']
+            sn=SNFO.cleaned_data['Sname']
+            sfn=SNFO.cleaned_data['S_F_name']
+            pn=SNFO.cleaned_data['Phone_No']
+            email=SNFO.cleaned_data['Email']
+            ad=SNFO.cleaned_data['Address']
+            SO=Student.objects.get_or_create(School=sc,Teachers=tc,Sname=sn,S_F_name=sfn,Phone_No=pn,Email=email,Address=ad)[0]
+            SO.save()
+            SD=Student.objects.all()
+            c={'SD':SD}
+            return render(request,'studentdata.html',c)
+        else:
+            return HttpResponse('Data is invalid')
+                  
     return render(request,'student.html',d)
