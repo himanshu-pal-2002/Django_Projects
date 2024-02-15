23299241a2ab1563e7798f87c2278c8058f7d04c
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-15 22:43:40 +0530

    listview

--- listview/listview/app/views.py
+++ listview/listview/app/views.py
@@ -3,27 +3,28 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import *
 
-# Create your views here.
+# Views For List View:
 class list(ListView):
     model=School
     context_object_name='schools'
 
+# Views For Detail View:
 class SchoolDetail(DetailView):
     model = School
     context_object_name='sclobject'
 
+# Views For Create View:
 class SchoolCreate(CreateView):
-
     model = School
     fields='__all__'
 
+# Views For Update View:
 class SchoolUpdate(UpdateView):
-
     model = School
     fields = '__all__'
 
+# Views For Delete View:
 class SchoolDelete(DeleteView):
-
     model  = School
     context_object_name = 'schoolobject'
     success_url=reverse_lazy('list')
