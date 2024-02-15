9379b293e5d2c3e2d677aea1bf98259063629c41
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-15 22:45:39 +0530

    listview

--- listview/listview/app/models.py
+++ listview/listview/app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
-# Create Model for School:
+# Model for Schools:
 class School(models.Model):
 
     sname  = models.CharField(max_length=100)
     sprincipal  = models.CharField(max_length=100)
     slocation  = models.CharField(max_length=100)
+
 
     def get_absolute_url(self):
         return reverse('detail',kwargs={'pk':self.pk})
@@ -15,14 +16,12 @@ class School(models.Model):
     def __str__(self):
         return self.sname
 
-# Create model for Students.
-    
+# Model for Students.  
 class Student(models.Model):
 
     sname  = models.CharField(max_length=100)
     sage  = models.IntegerField()
     Sname = models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
-
 
     def __str__(self):
         return self.sname
