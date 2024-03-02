3f14fd3c650201d91856813047fef76d4de29e6e
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-02 21:53:46 +0530

    DRF

--- DRF/project1/app/views.py
+++ DRF/project1/app/views.py
@@ -4,6 +4,7 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+# Views For API View for GET AND POST Data:
 @api_view(['GET','POST'])
 def School_Json_Data(request):
     SOD = School.objects.all()
