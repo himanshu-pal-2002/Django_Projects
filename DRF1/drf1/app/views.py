4f4e4748d75623624b85590f02fc9b4e890ad95d
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-05 22:59:48 +0530

    drf

--- DRF1/drf1/app/views.py
+++ DRF1/drf1/app/views.py
@@ -5,7 +5,9 @@ from rest_framework.decorators import APIView
 
 # Create Views For CRED by using API views:
 class ProductCred(APIView):
+    # For getting the Data:
     def get(self,request,id):
         PDO = Product.objects.all()
         SDO = ProductModelSerializers(PDO, many=True)
         return Response(SDO.data)
+    
