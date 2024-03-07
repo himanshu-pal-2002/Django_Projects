6d0428bd5962d949ff8b88da7deccfa5ba56cf62
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-07 17:51:21 +0530

    drf

--- DRF3/modelviewset/app/views.py
+++ DRF3/modelviewset/app/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from app.models import *
 from app.serializers import *
 
+# Views for Product Crud Operation by using ModelViewSet:
 class ProductCrud(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializers
