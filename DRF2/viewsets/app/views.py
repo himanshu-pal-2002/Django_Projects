0cdfdda088ce8007612bf06f3545742d764c5545
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-07 12:43:50 +0530

    drf

--- DRF2/viewsets/app/views.py
+++ DRF2/viewsets/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from app.serializers import ProductModelSerializers
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from app.models import *
 
-# Create your views here.
+class ProductCrud(ViewSet):
+    def list(self,request):
+        LPO = Product.objects.all()
+        JPO = ProductModelSerializers(LPO,many=True)
+        return Response(JPO.data)
+
+
+
