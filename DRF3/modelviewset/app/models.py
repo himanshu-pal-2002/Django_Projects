c4f900559997a4f9dd17b4842f0ab3cc1f735cc0
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-08 22:19:54 +0530

    drf3

--- DRF3/modelviewset/app/models.py
+++ DRF3/modelviewset/app/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 # Create Models for Product Category:
-
 class Product_Category(models.Model):
-
     Product_category_id = models.IntegerField()
     Product_category_name = models.CharField(max_length=100)
 
@@ -11,9 +9,7 @@ class Product_Category(models.Model):
         return self.Product_category_name
 
 # Create Models for Product Category:
-
 class Product(models.Model):
-
     Product_Category = models.ForeignKey(Product_Category,on_delete = models.CASCADE)
     Product_id = models.CharField(max_length=100)
     Product_name = models.CharField(max_length=100)
