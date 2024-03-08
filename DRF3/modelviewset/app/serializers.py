585c0634b99ba9ec38e26e8ddbf83d71f495813d
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-03-08 22:11:46 +0530

    drf3

--- DRF3/modelviewset/app/serializers.py
+++ DRF3/modelviewset/app/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from app.models import *
 
-
+# Serializers for Product Model:
 class ProductModelSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
